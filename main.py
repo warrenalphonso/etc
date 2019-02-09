25613be2c03483ee5e1a6e497a64482fd890ac8f
Oliver <obryniarski@berkeley.edu>
2019-02-09 14:56:14 -0800

    s

--- main.py
+++ main.py
@@ -196,36 +196,37 @@ def master_trade(exchange, BOND, VALBZ, VALE, GS, MS, WFC, XLF):
     if pnl < -10000:
         return None
 
-    #check if buying ETF or its constituents is a good idea
-    #NEED TO IMMEDIATELY convert and sell so we don't have to keep track of price at which we bought it
-    if XLF and WFC and MS and GS and BOND:
+    else:
+        #check if buying ETF or its constituents is a good idea
+        #NEED TO IMMEDIATELY convert and sell so we don't have to keep track of price at which we bought it
+        if XLF and WFC and MS and GS and BOND:
 
-        XLF_p = mean(XLF)
-        BOND_p = mean(BOND)
-        GS_p = mean(GS)
-        MS_p = mean(MS)
-        WFC_p = mean(WFC)
-        etf_strat = checkETF(XLF_p, BOND_p, GS_p, MS_p, WFC_p)
-        if etf_strat == "buyxlf":
-            write_to_exchange(exchange, new_buy_order('XLF', XLF_p + 1, 20)[1]) #we need to think about how sell order works do i need to store the id??
-            write_to_exchange(exchange, new_convert_order('XLF', 'SELL', xlf_own - (xlf_own % 10))[1])
-            write_to_exchange(exchange, new_sell_order('BOND', BOND_p - 1, bond_own)[1])
-            write_to_exchange(exchange, new_sell_order('GS', GS_p - 1, gs_own)[1])
-            write_to_exchange(exchange, new_sell_order('MS', MS_p - 1, ms_own)[1])
-            write_to_exchange(exchange, new_sell_order('WFC', WFC_p - 1, wfc_own)[1])
-        elif etf_strat == "buysum":
-            write_to_exchange(exchange, new_buy_order('BOND', BOND_p + 1, 6)[1])
-            write_to_exchange(exchange, new_buy_order('GS', GS_p + 1, 4)[1])
-            write_to_exchange(exchange, new_buy_order('MS', MS_p + 1, 6)[1])
-            write_to_exchange(exchange, new_buy_order('WFC', WFC_p + 1, 4)[1])
-            num_XLF_to_buy = min(bond_own / .3, gs_own / .2, ms_own / .3, wfc_own / .2) // 1
-            write_to_exchange(exchange, new_convert_order('XLF', 'BUY', num_XLF_to_buy)[1])
-            write_to_exchange(exchange, new_sell_order('XLF', XLF_p - 1, xlf_own)[1])
+            XLF_p = mean(XLF)
+            BOND_p = mean(BOND)
+            GS_p = mean(GS)
+            MS_p = mean(MS)
+            WFC_p = mean(WFC)
+            etf_strat = checkETF(XLF_p, BOND_p, GS_p, MS_p, WFC_p)
+            if etf_strat == "buyxlf":
+                write_to_exchange(exchange, new_buy_order('XLF', XLF_p + 1, 20)[1]) #we need to think about how sell order works do i need to store the id??
+                write_to_exchange(exchange, new_convert_order('XLF', 'SELL', xlf_own - (xlf_own % 10))[1])
+                write_to_exchange(exchange, new_sell_order('BOND', BOND_p - 1, bond_own)[1])
+                write_to_exchange(exchange, new_sell_order('GS', GS_p - 1, gs_own)[1])
+                write_to_exchange(exchange, new_sell_order('MS', MS_p - 1, ms_own)[1])
+                write_to_exchange(exchange, new_sell_order('WFC', WFC_p - 1, wfc_own)[1])
+            elif etf_strat == "buysum":
+                write_to_exchange(exchange, new_buy_order('BOND', BOND_p + 1, 6)[1])
+                write_to_exchange(exchange, new_buy_order('GS', GS_p + 1, 4)[1])
+                write_to_exchange(exchange, new_buy_order('MS', MS_p + 1, 6)[1])
+                write_to_exchange(exchange, new_buy_order('WFC', WFC_p + 1, 4)[1])
+                num_XLF_to_buy = min(bond_own / .3, gs_own / .2, ms_own / .3, wfc_own / .2) // 1
+                write_to_exchange(exchange, new_convert_order('XLF', 'BUY', num_XLF_to_buy)[1])
+                write_to_exchange(exchange, new_sell_order('XLF', XLF_p - 1, xlf_own)[1])
 
-        elif etf_strat == "buynone":
-            print('dont do etf')
+            elif etf_strat == "buynone":
+                print('dont do etf')
 
-        #check if selling ETF or its constituents
+            #check if selling ETF or its constituents
 
 
 
