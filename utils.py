16ed31cd328b09e7a6942c8a69f799171ee9e2c8
Oliver <obryniarski@berkeley.edu>
2019-02-09 11:44:54 -0800

    changed nonlocal to global

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ cur_order_id = 0
 
 
 def new_buy_order(stock, price, num_bought):
-    nonlocal cur_order_id
+    global cur_order_id
     order_dict = {'type': 'add', 'order_id': cur_order_id, "symbol": stock, 'dir': 'BUY',
                   'price': price, 'size': num_bought}
     cur_order_id += 1
@@ -12,7 +12,7 @@ def new_buy_order(stock, price, num_bought):
 
 
 def new_sell_order(stock, price, num_bought):
-    nonlocal cur_order_id
+    global cur_order_id
     order_dict = {'type': 'add', 'order_id': cur_order_id, "symbol": stock, 'dir': 'SELL',
                   'price': price, 'size': num_bought}
     cur_order_id += 1
