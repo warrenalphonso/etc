1d20772c4525d0ab940ae43d4db2aae784f70910
Warren Alphonso <warrenalphonso@berkeley.edu>
2019-02-09 12:51:02 -0800

    warren

--- test_algorithms.py
+++ test_algorithms.py
@@ -7,10 +7,11 @@ def master_trade(exchange, BOND, VALBZ, VALE, GS, MS, WFC, XLF):
     print('master')
 
 
-#pennying for trade bond 
+#pennying for trade bond
 def trade_bond(exchange):
     bond_buy.append(cur_order_id)
     write_to_exchange(exchange, new_buy_order('BOND', 999, 10))
     write_to_exchange(exchange, new_sell_order('BOND', 1000, 10))
 
 def ETFArbitrage(exchange, XLF, BOND, GS, MS, WFC):
+    
